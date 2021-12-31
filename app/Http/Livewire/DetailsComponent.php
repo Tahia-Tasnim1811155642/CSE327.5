f1969811a37a2c6fa7fd39ce9dc4ce32794b22d8
Moshiure-Rahman <moshiure.rahman@northsouth.edu>
2021-12-31 18:13:14 +0600

    Controller Part and Project setup file

---
+++ app/Http/Livewire/DetailsComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Product;
+use Cart;
+
+class DetailsComponent extends Component
+{
+    public $slug;
+
+    public function mount($slug){
+        $this->slug = $slug;
+    } 
+    public function store($product_id, $product_name, $product_price){
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
+        session()->flash('success_message', 'Item added in the cart');  
+        return redirect()->route('product.cart');
+     }
+    public function render()
+    {
+        $product = Product::where('slug', $this->slug)->first();
+        return view('livewire.details-component',['product'=>$product])->layout('layouts.base');
+    }
+}
