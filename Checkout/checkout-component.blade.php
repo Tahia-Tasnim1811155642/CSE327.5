7d3c3f010f4e51364c6540888ca7b1ef945e3963
Mushfiqur1 <87086863+Mushfiqur1@users.noreply.github.com>
2022-01-02 22:36:22 +0600

    Function

---
+++ Checkout/checkout-component.blade.php
@@ -0,0 +1,66 @@
+<main id="main" class="main-site">
+
+		<div class="container">
+
+			<div class="wrap-breadcrumb">
+				<ul>
+					<li class="item-link"><a href="#" class="link">home</a></li>
+					<li class="item-link"><span>login</span></li>
+				</ul>
+			</div>
+			<div class=" main-content-area">
+				<div class="wrap-address-billing">
+					<h3 class="box-title">Billing Address</h3>
+					<form action="#" method="get" name="frm-billing">
+						<p class="row-in-form">
+							<label for="fname">first name</label>
+							<input id="fname" type="text" name="fname" value="" placeholder="Your name">
+						</p>
+						<p class="row-in-form">
+							<label for="lname">last name</label>
+							<input id="lname" type="text" name="lname" value="" placeholder="Your last name">
+						</p>
+						<p class="row-in-form">
+							<label for="email">Email Addreess:</label>
+							<input id="email" type="email" name="email" value="" placeholder="Type your email">
+						</p>
+						<p class="row-in-form">
+							<label for="phone">Phone number</label>
+							<input id="phone" type="number" name="phone" value="" placeholder="10 digits format">
+						</p>
+						<p class="row-in-form">
+							<label for="add">Address:</label>
+							<input id="add" type="text" name="add" value="" placeholder="Street at apartment number">
+						</p>
+						<p class="row-in-form">
+							<label for="city">Town / City</label>
+							<input id="city" type="text" name="city" value="" placeholder="City name">
+						</p>
+					</form>
+				</div>
+				<div class="summary summary-checkout">
+					<div class="summary-item payment-method">
+						<h4 class="title-box">Payment Method</h4>
+						<div class="choose-payment-methods">
+							<label class="payment-method">
+								<input name="payment-method" id="payment-method-bank" value="bank" type="radio">
+								<span>Bkash</span>
+							</label>
+							<label class="payment-method">
+								<input name="payment-method" id="payment-method-visa" value="visa" type="radio">
+								<span>Rocket</span>
+							</label>
+							<label class="payment-method">
+								<input name="payment-method" id="payment-method-paypal" value="paypal" type="radio">
+								<span>Nagad</span>
+							</label>
+						</div>
+						<p class="summary-info grand-total"><span>Grand Total</span> <span class="grand-total-price">$100.00</span></p>
+						<a href="thankyou.html" class="btn btn-medium">Place order now</a>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+	</main>
